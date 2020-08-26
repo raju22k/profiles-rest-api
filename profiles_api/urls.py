99566ea2efcf232c9638ae020a40d7b6389bce60
Raju NG <raju22k@yahoo.com>
2020-08-26 17:42:08 +0530

    Added login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,5 +8,6 @@ router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
         path('hello-view/',views.HelloAPIView.as_view()),
+        path('login/',views.UserLoginApiView.as_view()),
         path('',include(router.urls)), # access by - http://127.0.0.1:8000/api/**/ or
 ]
